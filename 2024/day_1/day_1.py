4d1e41c2737364edda6bca3a88f345c6a8acb021
Varun <vasahni@gmail.com>
2025-08-20 20:36:13 -0400

    add similarity calculation

--- 2024/day_1/day_1.py
+++ 2024/day_1/day_1.py
@@ -1,3 +1,6 @@
+from collections import Counter
+
+
 def read_lists() -> tuple[list[int], list[int]]:
     with open("day_1_input.txt", "r") as f:
         left, right = [], []
@@ -7,14 +10,27 @@ def read_lists() -> tuple[list[int], list[int]]:
     return left, right
 
 
+def calculate_difference(left: list[int], right: list[int]) -> int:
+    difference = sum((abs(x - y) for x, y in zip(sorted(left), sorted(right))))
+    return difference
+
+
+def calculate_similarity(left: list[int], right: list[int]) -> int:
+    element_count = Counter(right)
+    sum = 0
+    for each in left:
+        sum += each * element_count[each]
+    return sum
+
+
 def run():
     left, right = read_lists()
     if len(left) != len(right):
         raise ValueError(
             f"Expected equal list length, but got left:{len(left)}, right:{len(right)}"
         )
-    difference = sum((abs(x - y) for x, y in zip(sorted(left), sorted(right))))
-    print(f"Total difference: {difference}")
+    print(f"Difference: {calculate_difference(left, right)}")
+    print(f"Similarity: {calculate_similarity(left, right)}")
 
 
 if __name__ == "__main__":

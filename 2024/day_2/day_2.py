a4869fddfdf8b35190b712442595d2a10a51adeb
Varun <vasahni@gmail.com>
2025-08-22 08:59:44 -0400

    update to use dampner

--- 2024/day_2/day_2.py
+++ 2024/day_2/day_2.py
@@ -1,3 +1,4 @@
+import sys
 from enum import Enum, auto
 
 
@@ -22,18 +23,33 @@ def check_differences(row: list[int]) -> bool:
     return all(1 <= abs(row[i] - row[i + 1]) <= 3 for i in range(len(row) - 1))
 
 
-def check_safety(row: list[int]) -> Safety:
+def dampen(row: list[int]) -> Safety:
+    for i in range(len(row)):
+        copied_row = row.copy()
+        copied_row.pop(i)
+        if check_differences(copied_row) and is_monotonic(copied_row):
+            return Safety.SAFE
+    return Safety.UNSAFE
+
+
+def run_safety(row: list[int], use_dampner: bool) -> Safety:
     if check_differences(row) and is_monotonic(row):
         return Safety.SAFE
-    return Safety.UNSAFE
+    elif use_dampner:
+        return dampen(row)
+    else:
+        return Safety.UNSAFE
 
 
 def run():
     safe = 0
+    bool_map = {"true": True, "false": False}
+    use_dampner = sys.argv[1:][0]
+    use_dampner = bool_map.get(use_dampner.lower())
     with open("input.txt", "r") as f:
         for line in f:
             row = list(map(int, line.strip().split(" ")))
-            if check_safety(row) == Safety.SAFE:
+            if run_safety(row, use_dampner) == Safety.SAFE:
                 safe += 1
     print(f"Total safe: {safe}")
 

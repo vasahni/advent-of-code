6ef30a63e9118e9b82d0a1c8c963e86b4b40ee41
Varun <vasahni@gmail.com>
2025-08-25 17:56:30 -0400

    handle second case for part 2

--- 2024/day_3/run.py
+++ 2024/day_3/run.py
@@ -1,20 +1,37 @@
 import re
 
 
-def match_cases(line: str) -> list[str]:
-    pattern = "mul\(([0-9]{1,3}),([0-9]{1,3})\)"
+def parse_line(line: str) -> list[str]:
+    pattern = "(?:mul\([0-9]{1,3},[0-9]{1,3}\)|do\(\)|don't\(\))"
     cases = re.findall(pattern, line)
     return cases
+
+
+def process_array(line: list[str], include: bool) -> tuple[int, bool]:
+    total = 0
+    for item in line:
+        if item == "don't()":
+            include = False
+        elif item == "do()":
+            include = True
+        else:
+            if include is False:
+                continue
+            left = int(item.split(",")[0].replace("mul(", ""))
+            right = int(item.split(",")[1].replace(")", ""))
+            product = left * right
+            total += product
+    return (total, include)
 
 
 def run():
     with open("input.txt", "r") as f:
         total = 0
+        include = True
         for line in f:
-            cases = match_cases(line)
-            list_total = sum((map(lambda x: int(x[0]) * int(x[1]), cases)))
-            total += list_total
-    print(total)
+            case = parse_line(line)
+            sub_total, include = process_array(case, include)
+            total += sub_total
 
 
 if __name__ == "__main__":
